882386c3e4e3f977d59db92b6c6cc0b755e8d256
Denis Piyack <denispiyack@gmail.com>
2026-02-05 14:02:32 +0100

    fix: unignore prisma schema for deployment

---
+++ prisma/migrations/20260131035411_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "name" TEXT NOT NULL,
+    "avatar" TEXT,
+    "discordId" TEXT,
+    "refreshToken" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_discordId_key" ON "users"("discordId");
