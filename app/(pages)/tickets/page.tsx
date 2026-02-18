c35ef758be5d4557d9268cca5dae24f09ff25428
Denis Piyack <denispiyack@gmail.com>
2026-02-18 18:05:05 +0100

    feat: prohibition of correspondence without permission

--- app/(pages)/tickets/page.tsx
+++ app/(pages)/tickets/page.tsx
@@ -1,9 +1,13 @@
 import { getTicketsCurrentUser } from '@/utils/api-server/tickets-server';
 import { getCurrentUser } from '@/utils/api-server/auth-server';
 import TicketsPageClient from '@/app/(components)/tickets-page/TicketsPageClient';
+import { redirect } from 'next/navigation';
 
 export default async function TicketsPage() {
   const [tickets, currentUser] = await Promise.all([getTicketsCurrentUser(), getCurrentUser()]);
+  if (!currentUser) {
+    redirect('/auth');
+  }
 
   return <TicketsPageClient initialTickets={tickets} currentUser={currentUser} />;
 }
