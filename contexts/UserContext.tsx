474e31ea12748cef9d08e1b5ae33285b1066c6c9
Denis Piyack <denispiyack@gmail.com>
2026-02-20 22:20:09 +0100

    feat: adding context to remove extra props in components

---
+++ contexts/UserContext.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import { User } from '@/types/user';
+
+interface UserContextType {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export function UserProvider({
+  children,
+  initialUser,
+}: {
+  children: ReactNode;
+  initialUser: User | null;
+}) {
+  const [user, setUser] = useState<User | null>(initialUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+}
+
+export default function useUser() {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
+}
