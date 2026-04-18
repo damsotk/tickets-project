cf9b4214e313cf7fc61fae43ac92feb03383a799
Denis Piyack <denispiyack@gmail.com>
2026-04-18 05:17:20 +0200

    refactor: rollback to simple metadata before drawing a normal picture

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,25 +8,8 @@ import CookieConsentBanner from '../(components)/CookieConsentBanner';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://elium.site'),
-  title: {
-    default: 'Library of Knowledge',
-    template: '%s | Library of Knowledge',
-  },
-  description: 'Explore the vast world of lore, characters, cities, and faith.',
-  openGraph: {
-    type: 'website',
-    siteName: 'Library of Knowledge',
-    locale: 'en_US',
-    images: [
-      {
-        url: '/ellium-tickets-images/logo.png',
-        width: 1200,
-        height: 630,
-        alt: 'Library of Knowledge',
-      },
-    ],
-  },
+  title: 'Elium',
+  description: 'Minecraft roleplay server',
 };
 
 export default async function LocaleLayout({
