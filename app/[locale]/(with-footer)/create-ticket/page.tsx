d18571a73a9f8cac72dde2818059213bb5010b15
Denis Piyack <denispiyack@gmail.com>
2026-04-18 05:42:32 +0200

    refactor: delete wrong metadata, add favicon

--- app/[locale]/(with-footer)/create-ticket/page.tsx
+++ app/[locale]/(with-footer)/create-ticket/page.tsx
@@ -1,11 +1,5 @@
-import type { Metadata } from 'next';
 import styles from '@/app/(styles)/create-ticket-styles/main-page.module.css';
 import TicketsTypes from '@/app/(components)/create-ticket-page/TicketsTypes';
-
-export const metadata: Metadata = {
-  title: 'Ellium Tickets',
-  description: 'Application for create tickets',
-};
 
 export default async function CreateTicketPage() {
   return (
--- app/[locale]/(with-footer)/page.tsx
+++ app/[locale]/(with-footer)/page.tsx
@@ -1,14 +1,8 @@
-import type { Metadata } from 'next';
 import styles from '@/app/(styles)/main-page.module.css';
 import HeroSection from '../../(components)/main-page/HeroSection';
 import OnlineStats from '../../(components)/main-page/OnlineStats';
 import NavigationGrid from '../../(components)/main-page/NavigationGrid';
 import WhiteList from '@/app/(components)/main-page/WhiteList';
-
-export const metadata: Metadata = {
-  title: 'Ellium',
-  description: 'Ellium Minecraft Server',
-};
 
 export default function ElliumMain() {
   return (
